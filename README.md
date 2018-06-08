9fbd966f2d35cc102f71aeb97a6e5ce6cc417623
David malý <maly.david.it@gmail.com>
2018-06-08 16:20:19 +0200

    Working application Added resources First draft of Readme

---
+++ README.md
@@ -0,0 +1,19 @@
+﻿# TrayIcon
+WPF aplikace, která pomocí Windows Forms knihovny zobrazuje ikonu běžící aplikace na hlavním panelu operačního systému Windows (vedle hodin). Ukazuje konfiguraci ikony a kontextové nabídky.
+![Result](Resources/Result.png)
+
+## Nastavení projektu
+Pro využítí Windows Forms je potřeba přidat do projektu referenci:
+![Win Forms Reference](Resources/WinFormsReference.png)
+
+Ikonu je vhodné vložit do projektu jako zdroj (resource) aplikace:
+![Icon](Resources/Icon.png)
+
+Poté k ní lze přistoupit přímo z kódu:
+```csharp
+System.Windows.Forms.NotifyIcon _notifyIcon;
+_notifyIcon.Icon = TrayIcon.Properties.Resources.Icon;
+```
+> TrayIcon je název namespace, ten je třeba změnit dle názvu Assebmly
+
+## Aplikace 
--- WPF.TrayIcon/App.xaml.cs
+++ WPF.TrayIcon/App.xaml.cs
@@ -1,17 +1,77 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Resources;
 using System.Threading.Tasks;
 using System.Windows;
 
 namespace WPF.TrayIcon
 {
-    /// <summary>
-    /// Interakční logika pro App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        private bool _isExit;
+        private System.Windows.Forms.NotifyIcon _notifyIcon;
+
+        /// <summary>
+        /// Set icon and context menu on application startup and show main window of application
+        /// </summary>
+        /// <param name="e">Startup arguments</param>
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            
+            MainWindow = new MainWindow();
+            MainWindow.Closing += MainWindow_Closing;
+
+            _notifyIcon = new System.Windows.Forms.NotifyIcon();
+            _notifyIcon.DoubleClick += (s, args) => ShowMainWindow();
+            // Set application icon
+            _notifyIcon.Icon = TrayIcon.Properties.Resources.Icon;
+            _notifyIcon.Visible = true;
+
+            CreateContextMenu();
+        }
+
+        private void CreateContextMenu()
+        {
+            _notifyIcon.ContextMenuStrip =
+                new System.Windows.Forms.ContextMenuStrip();
+            _notifyIcon.ContextMenuStrip.Items.Add("MainWindow...").Click += (s, e) => ShowMainWindow();
+            _notifyIcon.ContextMenuStrip.Items.Add("Exit").Click += (s, e) => ExitApplication();
+        }
+
+        private void ExitApplication()
+        {
+            _isExit = true;
+            MainWindow.Close();
+            _notifyIcon.Dispose();
+            _notifyIcon = null;
+        }
+
+        private void ShowMainWindow()
+        {
+            if (MainWindow.IsVisible)
+            {
+                if (MainWindow.WindowState == WindowState.Minimized)
+                    MainWindow.WindowState = WindowState.Normal;
+                MainWindow.Activate();
+            }
+            else
+            {
+                MainWindow.Show();
+            }
+        }
+
+        private void MainWindow_Closing(object sender, CancelEventArgs e)
+        {
+            if (!_isExit)
+            {
+                e.Cancel = true;
+                MainWindow.Hide(); // A hidden window can be shown again, a closed one not
+            }
+        }
     }
 }
